62d4b925cb7b3aa8171d664bf2268e3c28c4d7f1
Mark Gardner <mark.gardner@endurance.com>
2019-03-22 14:36:27 -0500

    first draft of models

---
+++ models/comment.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+  var Comment = sequelize.define("Comment", {
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      defaultValue: 0,
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 5
+      }
+    },
+    authorEmail: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true
+      }
+    }
+  });
+
+  Comment.associate = function(models) {
+    Comment.belongsTo(models.Project, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Comment;
+};
