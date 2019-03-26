4a9c73df1b20fce75b6f89727c09b66bb7e54a7a
Mark Gardner <mark.gardner@endurance.com>
2019-03-25 20:19:41 -0500

    add fulltext indices to project name and description

--- models/project.js
+++ models/project.js
@@ -1,29 +1,44 @@
 module.exports = function(sequelize, DataTypes) {
-  var Project = sequelize.define("Project", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        notEmpty: true
+  var Project = sequelize.define(
+    "Project",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+          notEmpty: true
+        }
+      },
+      pictureUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          isUrl: true
+        }
+      },
+      visits: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        allowNull: false,
+        defaultValue: 0
       }
     },
-    pictureUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        isUrl: true
-      }
-    },
-    visits: {
-      type: DataTypes.BIGINT.UNSIGNED,
-      allowNull: false,
-      defaultValue: 0
+    {
+      indexes: [
+        {
+          type: "FULLTEXT",
+          fields: ["name"]
+        },
+        {
+          type: "FULLTEXT",
+          fields: ["description"]
+        }
+      ]
     }
-  });
+  );
 
   Project.associate = function(models) {
     Project.hasMany(models.Comment, {
